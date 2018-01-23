425bd1a486e8fcb5918dad3f6fb71d8687d766e8
github username <github email address>
2018-01-23 10:59:22 -0500

    rounds posting

--- app/controllers/api/v1/rounds_controller.rb
+++ app/controllers/api/v1/rounds_controller.rb
@@ -7,13 +7,12 @@ class Api::V1::RoundsController < ApplicationController
 
   def create
     @round = Round.create(round_params)
-    byebug
     render json: @round
   end
 
   private
   def round_params
-    params.permit(:user1_id, :user1_score, :user2_id, :user1_score, :category)
+    params.require(:round).permit(:user1_id, :user1_score, :user2_id, :user2_score, :game_id, :category)
 
   end
 end
